a60813c1e913ae50cf50957553ede74309fe08f8
PeterBrain <peter.loecker@live.at>
2017-08-06 20:17:10 +0200

    Smoke, Motion & Occupacy
    Added support for Smoke, Motion & Occupacy
    Converted brightness & color service "if" to "switch"
    Code cleanup

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ function HttpAccessory(log, config) {
 	this.http_hue_method = config["http_hue_method"] || this.http_method;
 	this.http_saturation_method = config["http_saturation_method"] || this.http_method;
 	
-	// switch (also light)
+	// switch, light and non-switch handling accessories (sensors)
 	this.service = config["service"] || "Switch";
 	this.switchHandling = config["switchHandling"] || "no";
-	this.on_body = config["on_body"];
 	this.on_url = config["on_url"];
+	this.on_body = config["on_body"];
 	this.off_url = config["off_url"];
 	this.off_body = config["off_body"];
 	this.status_url = config["status_url"];
@@ -60,12 +60,13 @@ function HttpAccessory(log, config) {
 	
 	var that = this;
 
+	
 	// Status Polling, if you want to add additional services that don't use switch handling you can add something like this || (this.service=="Smoke" || this.service=="Motion"))
-	if (this.status_url && this.switchHandling === "realtime") {
+	if ((this.status_url && this.switchHandling === "realtime") || (this.service=="Occupancy") || (this.service=="Smoke" || this.service=="Motion")) {
 		
 		var powerurl = this.status_url;
-		var statusemitter = pollingtoevent(function (done) {
-			that.httpRequest(powerurl, "", "GET", that.username, that.password, that.sendimmediately, function (error, response, body) {
+		var statusemitter = pollingtoevent(function(done) {
+			that.httpRequest(powerurl, "", "GET", that.username, that.password, that.sendimmediately, function(error, response, body) {
 				if (error) {
 					that.log("HTTP get power function failed: %s", error.message);
 					try {
@@ -93,7 +94,7 @@ function HttpAccessory(log, config) {
 			return objectsEqual;
 		}
 
-		statusemitter.on("statuspoll", function (responseBody) {
+		statusemitter.on("statuspoll", function(responseBody) {
 			var binaryState;
 			
 			if (that.status_on && that.status_off) {//Check if custom status checks are set
@@ -112,7 +113,7 @@ function HttpAccessory(log, config) {
 				
 				that.log("Status On Status Poll", statusOn);
 				if (statusOn) binaryState = 1;
-				// else binaryState = 0;
+				//else binaryState = 0;
 				if (statusOff) binaryState = 0;
 			} else {
 				binaryState = parseInt(responseBody.replace(/\D/g, ""));
@@ -138,6 +139,27 @@ function HttpAccessory(log, config) {
 							.setValue(that.state);
 					}
 				break;
+				case "Smoke":
+					if (that.smokeService) {
+						that.smokeService
+							.getCharacteristic(Characteristic.SmokeDetected)
+							.setValue(that.state);
+					}
+				break;
+				case "Motion":
+					if (that.motionService) {
+						that.motionService
+							.getCharacteristic(Characteristic.MotionDetected)
+							.setValue(that.state);
+					}
+				break;
+				case "Occupancy":
+					if (that.motionService) {
+						that.motionService
+							.getCharacteristic(Characteristic.OccupancyDetected)
+							.setValue(that.state);
+					}
+				break;
 			}
 			
 			that.enableSet = true;
@@ -149,8 +171,8 @@ function HttpAccessory(log, config) {
 	if (this.brightnesslvl_url && this.brightnessHandling === "realtime") {
 		var brightnessurl = this.brightnesslvl_url;
 		
-		var levelemitter = pollingtoevent(function (done) {
-			that.httpRequest(brightnessurl, "", "GET", that.username, that.password, that.sendimmediately, function (error, response, responseBody) {
+		var levelemitter = pollingtoevent(function(done) {
+			that.httpRequest(brightnessurl, "", "GET", that.username, that.password, that.sendimmediately, function(error, response, responseBody) {
 				if (error) {
 					that.log("HTTP get power function failed: %s", error.message);
 					return;
@@ -160,7 +182,7 @@ function HttpAccessory(log, config) {
 			})// set longer polling as slider takes longer to set value
 		}, { longpolling: true, interval: 300, longpollEventName: "levelpoll" });
 
-		levelemitter.on("levelpoll", function (responseBody) {
+		levelemitter.on("levelpoll", function(responseBody) {
 			that.currentlevel = parseInt(responseBody);
 			that.enableSet = false;
 			
@@ -175,9 +197,8 @@ function HttpAccessory(log, config) {
 		});
 	}
 	
-	//#######################################################################
 	// Hue Polling
-	if (this.hue_url && this.colorHandling =="realtime") {
+	if (this.hue_url && this.colorHandling === "realtime") {
 		var hueurl = this.hue_url;
 		var levelemitter = pollingtoevent(function(done) {
 			that.httpRequest(hueurl, "", "GET", that.username, that.password, that.sendimmediately, function(error, response, responseBody) {
@@ -203,7 +224,7 @@ function HttpAccessory(log, config) {
 	}
 
 	// Saturation Polling
-	if (this.saturation_url && this.colorHandling =="realtime") {
+	if (this.saturation_url && this.colorHandling === "realtime") {
 		var saturationurl = this.saturation_url;
 		var levelemitter = pollingtoevent(function(done) {
 			that.httpRequest(saturationurl, "", "GET", that.username, that.password, that.sendimmediately, function(error, response, responseBody) {
@@ -227,11 +248,12 @@ function HttpAccessory(log, config) {
 			}        
 		});
 	}
-	//#######################################################################
 }
 
+
+
 HttpAccessory.prototype = {
-	httpRequest: function (url, body, method, username, password, sendimmediately, callback) {
+	httpRequest: function(url, body, method, username, password, sendimmediately, callback) {
 		request({
 			url: url,
 			body: body,
@@ -243,51 +265,13 @@ HttpAccessory.prototype = {
 				sendImmediately: sendimmediately
 			}
 		},
-		function (error, response, body) {
+		function(error, response, body) {
 			callback(error, response, body)
 		})
 	},
 
-	setPowerState: function (powerState, callback) {
-		this.log("Power On", powerState);
-		this.log("Enable Set", this.enableSet);
-		this.log("Current Level", this.currentlevel);
-		
-		if (this.enableSet === true) {
-			var url;
-			var body;
-
-			if (!this.on_url || !this.off_url) {
-				this.log.warn("Ignoring request; No power url defined.");
-				callback(new Error("No power url defined."));
-				return;
-			}
-
-			if (powerState) {
-				url = this.on_url;
-				body = this.on_body;
-				this.log("Setting power state to on");
-			} else {
-				url = this.off_url;
-				body = this.off_body;
-				this.log("Setting power state to off");
-			}
-
-			this.httpRequest(url, body, this.http_method, this.username, this.password, this.sendimmediately, function (error, response, responseBody) {
-				if (error) {
-					this.log("HTTP set power function failed: %s", error.message);
-					callback(error);
-				} else {
-					this.log("HTTP set power function succeeded!");
-					callback();
-				}
-			}.bind(this));
-		} else {
-			callback();
-		}
-	},
-
-	getPowerState: function (callback) {
+	// for switch & sensor (smoke, motion, occupancy)
+	getPowerState: function(callback) {
 		if (!this.status_url) {
 			this.log.warn("Ignoring request; No status url defined.");
 			callback(new Error("No status url defined."));
@@ -333,8 +317,49 @@ HttpAccessory.prototype = {
 			}
 		}.bind(this));
 	},
+	
+	setPowerState: function(powerState, callback) {
+		this.log("Power On", powerState);
+		this.log("Enable Set", this.enableSet);
+		this.log("Current Level", this.currentlevel);
+		
+		if (this.enableSet === true) {
+			var url;
+			var body;
 
-	getBrightness: function (callback) {
+			if (!this.on_url || !this.off_url) {
+				this.log.warn("Ignoring request; No power url defined.");
+				callback(new Error("No power url defined."));
+				return;
+			}
+
+			if (powerState) {
+				url = this.on_url;
+				body = this.on_body;
+				this.log("Setting power state to on");
+			} else {
+				url = this.off_url;
+				body = this.off_body;
+				this.log("Setting power state to off");
+			}
+
+			this.httpRequest(url, body, this.http_method, this.username, this.password, this.sendimmediately, function(error, response, responseBody) {
+				if (error) {
+					this.log("HTTP set power function failed: %s", error.message);
+					callback(error);
+				} else {
+					this.log("HTTP set power function succeeded!");
+					callback();
+				}
+			}.bind(this));
+		} else {
+			callback();
+		}
+	},
+
+
+	// for light (brightness, hue, saturation)
+	getBrightness: function(callback) {
 		if (!this.brightnesslvl_url) {
 			this.log.warn("Ignoring request; No brightness level url defined.");
 			callback(new Error("No brightness level url defined."));
@@ -357,7 +382,7 @@ HttpAccessory.prototype = {
 		}.bind(this));
 	},
 
-	setBrightness: function (level, callback) {
+	setBrightness: function(level, callback) {
 		if (this.enableSet === true) {
 			if (!this.brightness_url) {
 				this.log.warn("Ignoring request; No brightness url defined.");
@@ -382,7 +407,6 @@ HttpAccessory.prototype = {
 		}
 	},
 	
-	//#######################################################################
 	getHue: function(callback) {
 		if (!this.hue_url) {
 			this.log.warn("Ignoring request; No hue level url defined.");
@@ -470,14 +494,14 @@ HttpAccessory.prototype = {
 			}
 		}.bind(this));
 	},
-	//#######################################################################
 
-	identify: function (callback) {
+
+	identify: function(callback) {
 		this.log("Identify requested!");
-		callback(); // success
+		callback(); //success
 	},
 
-	getServices: function () {
+	getServices: function() {
 		var that = this;
 
 		// you can OPTIONALLY create an information service if you wish to override
@@ -504,9 +528,7 @@ HttpAccessory.prototype = {
 					case "realtime":
 						this.switchService
 							.getCharacteristic(Characteristic.On)
-							.on("get", function (callback) {
-								callback(null, that.state)
-							})
+							.on("get", function (callback) {callback(null, that.state)})
 							.on("set", this.setPowerState.bind(this));
 					break;
 					default:
@@ -517,6 +539,7 @@ HttpAccessory.prototype = {
 				}
 				
 				return [this.switchService];
+			break;
 				
 			case "Light":
 				this.lightbulbService = new Service.Lightbulb(this.name);
@@ -532,9 +555,7 @@ HttpAccessory.prototype = {
 					case "realtime":
 						this.lightbulbService
 							.getCharacteristic(Characteristic.On)
-							.on("get", function (callback) {
-								callback(null, that.state)
-							})
+							.on("get", function(callback) {callback(null, that.state)})
 							.on("set", this.setPowerState.bind(this));
 					break;
 					default:
@@ -545,44 +566,73 @@ HttpAccessory.prototype = {
 				}
 				
 				// Brightness Polling
-				if (this.brightnessHandling === "realtime") {
-					this.lightbulbService
-						.addCharacteristic(new Characteristic.Brightness())
-						.on("get", function (callback) {
-							callback(null, that.currentlevel)
-						})
-						.on("set", this.setBrightness.bind(this));
-				} else if (this.brightnessHandling === "yes") {
-					this.lightbulbService
-						.addCharacteristic(new Characteristic.Brightness())
-						.on("get", this.getBrightness.bind(this))
-						.on("set", this.setBrightness.bind(this));
+				switch (this.brightnessHandling) {
+					case "yes":
+						this.lightbulbService
+							.addCharacteristic(new Characteristic.Brightness())
+							.on("get", this.getBrightness.bind(this))
+							.on("set", this.setBrightness.bind(this));
+					break;
+					case: "realtime":
+						this.lightbulbService
+							.addCharacteristic(new Characteristic.Brightness())
+							.on("get", function(callback) {callback(null, that.currentlevel)})
+							.on("set", this.setBrightness.bind(this));
+					break;
 				}
 				
-				//#######################################################################
-				// Color Polling 
-				if (this.colorHandling == "realtime") {
-					this.lightbulbService 
-						.addCharacteristic(new Characteristic.Saturation())
-						.on('get', function(callback) {callback(null, that.currentsaturation)})
-						.on('set', this.setSaturation.bind(this));
-					this.lightbulbService 
-						.addCharacteristic(new Characteristic.Hue())
-						.on('get', function(callback) {callback(null, that.currenthue)})
-						.on('set', this.setHue.bind(this));
-				} else if (this.colorHandling == "yes") {
-					this.lightbulbService
-						.addCharacteristic(new Characteristic.Saturation())
-						.on('get', this.getSaturation.bind(this))
-						.on('set', this.setSaturation.bind(this));	
-					this.lightbulbService
-						.addCharacteristic(new Characteristic.Hue())
-						.on('get', this.getHue.bind(this))
-						.on('set', this.setHue.bind(this));							
+				// Color Polling
+				switch (this.colorHandling) {
+					case "yes":
+						this.lightbulbService
+							.addCharacteristic(new Characteristic.Hue())
+							.on('get', this.getHue.bind(this))
+							.on('set', this.setHue.bind(this));
+						this.lightbulbService
+							.addCharacteristic(new Characteristic.Saturation())
+							.on('get', this.getSaturation.bind(this))
+							.on('set', this.setSaturation.bind(this));
+					break;
+					case: "realtime":
+						this.lightbulbService 
+							.addCharacteristic(new Characteristic.Hue())
+							.on('get', function(callback) {callback(null, that.currenthue)})
+							.on('set', this.setHue.bind(this));
+						this.lightbulbService
+							.addCharacteristic(new Characteristic.Saturation())
+							.on('get', function(callback) {callback(null, that.currentsaturation)})
+							.on('set', this.setSaturation.bind(this));
+					break;
 				}
-				//#######################################################################
 
 				return [informationService, this.lightbulbService];
+			break;
+				
+			case "Smoke":
+				this.smokeService = new Service.SmokeSensor(this.name);
+				this.switchHandling === "realtime";
+				this.smokeService
+					.getCharacteristic(Characteristic.SmokeDetected)
+					.on('get', function(callback) {callback(null, that.state)});
+				return [this.smokeService];
+			break;
+				
+			case "Motion":
+				this.motionService = new Service.MotionSensor(this.name);
+				this.switchHandling === "realtime";				
+				this.motionService
+					.getCharacteristic(Characteristic.MotionDetected)
+					.on('get', function(callback) {callback(null, that.state)});
+				return [this.motionService];
+			break;
+				
+			case "Occupancy":
+				this.motionService = new Service.OccupancySensor(this.name);
+				this.switchHandling === "realtime";                
+				this.motionService
+					.getCharacteristic(Characteristic.OccupancyDetected)
+					.on('get', function(callback) {callback(null, that.state)});
+				return [this.motionService];
 			break;
 		}
 	}
